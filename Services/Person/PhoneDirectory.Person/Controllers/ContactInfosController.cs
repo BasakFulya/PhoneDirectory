93526b0a9b7d1914f8f0ef2e92cc991e1c5a349a
basakfulya <basakfulyaytu@gmail.com>
2024-06-16 00:29:54 +0300

    Controllers are created.

---
+++ Services/Person/PhoneDirectory.Person/Controllers/ContactInfosController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhoneDirectory.Person.Dtos.ContactInfoDtos;
+using PhoneDirectory.Person.Services.ContactInfoService;
+
+namespace PhoneDirectory.Person.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactInfosController : ControllerBase
+    {
+        private readonly IContactInfoService _contactInfoService;
+
+        public ContactInfosController(IContactInfoService contactInfoService)
+        {
+            _contactInfoService = contactInfoService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ContactInfoList()
+        {
+            var values = await _contactInfoService.GetAllContactInfoAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetContactInfoById(string id)
+        {
+            var values = await _contactInfoService.GetByIdContactInfoAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrateContactInfo(CreateContactInfoDto createContactInfoDto)
+        {
+            await _contactInfoService.CreateContactInfoAsync(createContactInfoDto);
+            return Ok("İletişim bilgileri başarıyla eklendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteContactInfo(string id)
+        {
+            await _contactInfoService.DeleteContactInfoAsync(id);
+            return Ok("İletişim bilgileri başarıyla silindi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateContactInfo(UpdateContactInfoDto updateContactInfoDto)
+        {
+            await _contactInfoService.UpdateContactInfoAsync(updateContactInfoDto);
+            return Ok("İletişim bilgileri başarıyla güncellendi.");
+        }
+    }
+}
---
+++ Services/Person/PhoneDirectory.Person/Controllers/PersonsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhoneDirectory.Person.Dtos.PersonDtos;
+using PhoneDirectory.Person.Services.PersonService;
+
+namespace PhoneDirectory.Person.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IPersonService _personService;
+
+        public PersonsController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PersonList()
+        {
+            var values = await _personService.GetAllPersonAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPersonById(string id)
+        {
+            var values = await _personService.GetByIdPersonAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CratePerson(CreatePersonDto createPersonDto)
+        {
+            await _personService.CreatePersonAsync(createPersonDto);
+            return Ok("Kişi başarıyla eklendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeletePerson(string id)
+        {
+            await _personService.DeletePersonAsync(id);
+            return Ok("Kişi başarıyla silindi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatePerson(UpdatePersonDto updatePersonDto)
+        {
+            await _personService.UpdatePersonAsync(updatePersonDto);
+            return Ok("Kişi başarıyla güncellendi.");
+        }
+    }
+}

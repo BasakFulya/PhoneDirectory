5bdabf7d4514428d95abbe4d47868a2f402956dd
basakfulya <basakfulyaytu@gmail.com>
2024-06-15 23:16:31 +0300

    ContactInfoDtos are created

---
+++ Services/Person/PhoneDirectory.Person/Dtos/ContactInfoDtos/CreateContactInfoDto.cs
@@ -0,0 +1,11 @@
+﻿using PhoneDirectory.Person.Entities;
+
+namespace PhoneDirectory.Person.Dtos.ContactInfoDtos
+{
+    public class CreateContactInfoDto
+    {
+        public string PersonId { get; set; }
+        public ContactType Type { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Services/Person/PhoneDirectory.Person/Dtos/ContactInfoDtos/ResultContactInfoDto.cs
@@ -0,0 +1,12 @@
+﻿using PhoneDirectory.Person.Entities;
+
+namespace PhoneDirectory.Person.Dtos.ContactInfoDtos
+{
+    public class ResultContactInfoDto
+    {
+        public string ContactInfoId { get; set; }
+        public string PersonId { get; set; }
+        public ContactType Type { get; set; }
+        public string Content { get; set; }
+    }
+}

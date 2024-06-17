c20d662b2ed81cdfec72c0523e7b6cb981af59df
basakfulya <basakfulyaytu@gmail.com>
2024-06-17 23:35:20 +0300

    Kafka Producer Entegrations coded

---
+++ Services/Report/PhoneDirectory.Report/Services/KafkaService/KafkaProducerService.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+using System.Text.Json;
+
+namespace PhoneDirectory.Report.Services.KafkaService
+{
+    public class KafkaProducerService
+    {
+        private readonly string _topic;
+        private readonly IProducer<Null, string> _producer;
+
+        public KafkaProducerService(IConfiguration configuration)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = configuration["Kafka:BootstrapServers"]
+            };
+            _topic = configuration["Kafka:Topic"];
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+
+        public async Task SendReportRequestAsync()
+        {
+            await _producer.ProduceAsync(_topic, new Message<Null, string> { Value = "Report initialized!" });
+        }
+
+    }
+}
